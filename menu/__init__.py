4c91148234dae3e749dc16006f84e8bf2633837e
Zakariyah Abdel-Illah <zak@absvfx.me>
2019-08-28 11:30:44 +0100

    describe menu class

---
+++ menu/__init__.py
@@ -0,0 +1,8 @@
+'''
+hydro.core.menu is the class that controls hydro-related menu items around an attached DCC. With these menu items, a
+user will have the ability to interact with an object of a given context, with tasks that are able to accept and work
+with those contexts.
+'''
+
+class HydroMenu(object):
+    pass
